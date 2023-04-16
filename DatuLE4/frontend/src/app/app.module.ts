de9cbc38ff4e6a736da0c2862442c39011bd0c64
niltiac2Ds <caitlinDSda2@gmail.com>
2023-04-16 21:00:59 +0800

    LE5: Changes as of 04/16/2023 21:00

--- DatuLE4/frontend/src/app/app.module.ts
+++ DatuLE4/frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPostsComponent } from './components/list-posts/list-posts.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { ListPostsComponent } from './components/list-posts/list-posts.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
