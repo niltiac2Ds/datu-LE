27a8464469705b65e11174fc9a76665631f59e83
niltiac2Ds <caitlinDSda2@gmail.com>
2023-04-16 22:41:08 +0800

    LE5: Changes as of 04/16/2023 22:41

--- DatuLE4/frontend/src/app/app-routing.module.ts
+++ DatuLE4/frontend/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPostsComponent } from './components/list-posts/list-posts.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 const routes: Routes = [
   {path:'',component:ListPostsComponent},
   {path: 'posts/:id', component: PostDetailComponent},
-
+  {path:'login-page', component: LoginPageComponent},
+  {path: 'register-page', component: RegisterPageComponent}
 ];
 
 @NgModule({
