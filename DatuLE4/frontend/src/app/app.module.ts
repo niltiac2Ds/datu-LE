a23baf7aa24b6a06ed817435a3df67b6e318b3d1
niltiac2Ds <caitlinDSda2@gmail.com>
2023-04-17 13:53:46 +0800

    LE5: Changes as of 04/16/2023 13:53

--- DatuLE4/frontend/src/app/app.module.ts
+++ DatuLE4/frontend/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPostsComponent } from './components/list-posts/list-posts.component';
-
-import { HttpClientModule } from '@angular/common/http';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
